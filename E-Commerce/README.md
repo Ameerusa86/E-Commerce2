b897b14ef582245da129615656226a3c1afcf339
Ameerusa86 <engamermecha@gmail.com>
2023-02-26 20:53:03 -0500

    -

---
+++ E-Commerce/README.md
@@ -0,0 +1,9 @@
+# ▶️ Watch Full Playlist 👇
+
+[![Everything Is AWESOME](https://yt-embed.herokuapp.com/embed?v=55_EIL59sIU)](https://youtube.com/playlist?list=PLKxAYAE8gnTvc7wheQp-AYq2xlwms6UaB "Code With Yd")
+
+# Getting Started with Create React App
+
+Full Stack E-Commerce website
+
+![](./src/images/screenshot-.png)
---
+++ E-Commerce/src/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { render } from "react-dom";
+import "./index.css";
+import App from "./App";
+import { ThemeProvider } from "./GlobalComponents/ThemeProvider";
+import { CartProvider } from "react-use-cart";
+
+render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <CartProvider>
+        <App />
+      </CartProvider>
+    </ThemeProvider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
